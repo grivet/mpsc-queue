370465398b9fc65e8af09376e9e73fdba6854ff3
Gaetan Rivet <grive@u256.net>
2021-02-27 20:41:59 +0100

    test: add unit tests
    Add unit tests for:
    
     * Simple insertion
     * Partial insertion
     * Back insertion
    
    Signed-off-by: Gaetan Rivet <grive@u256.net>

--- Makefile
+++ Makefile
@@ -1,24 +1,33 @@
 all: run
 
-perf_OBJS := test/perf/ts-mpsc-queue.o
-perf_OBJS += test/perf/perf.o
-perf_OBJS += test/util.o
-
 CFLAGS := -pthread -std=c11 -MD -Wall -Wextra $(EXTRA_CFLAGS)
 CFLAGS += -I$(CURDIR) -I$(CURDIR)/test
 
 test/%.o: test/%.c Makefile
 	$(CC) $(CFLAGS) -c -o $@ $<
 
+unit_OBJS := test/unit/unit.o
+unit_OBJS += test/util.o
+
+unit: $(unit_OBJS)
+	$(CC) $(CFLAGS) -o $@ $^
+
+perf_OBJS := test/perf/perf.o
+perf_OBJS += test/util.o
+perf_OBJS += test/perf/ts-mpsc-queue.o
+
 perf: $(perf_OBJS)
 	$(CC) $(CFLAGS) -o $@ $^
 
 .PHONY: run
-run: perf
-	$(WRAPPER) $(CURDIR)/perf -n 10000000 -c $$(nproc)
+run: unit perf
+	$(WRAPPER) $(CURDIR)/unit \
+	&& $(WRAPPER) $(CURDIR)/perf -n 10000000 -c $$(nproc)
 
 -include test/perf/*.d
+-include test/unit/*.d
 
 .PHONY: clean
 clean:
-	rm -f $(perf_OBJS) $(perf_OBJS:%.o=%.d) perf
+	rm -f unit $(unit_OBJS) $(unit_OBJS:%.o=%.d)
+	rm -f perf $(perf_OBJS) $(perf_OBJS:%.o=%.d)
---
+++ test/unit/unit.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#include "mpsc-queue.h"
+#include "util.h"
+
+struct element {
+    unsigned int id;
+    struct mpsc_queue_node node;
+};
+
+static struct mpsc_queue *
+mq_create(void)
+{
+    struct mpsc_queue *q = xmalloc(sizeof(*q));
+    mpsc_queue_init(q);
+    return q;
+}
+
+static void
+mq_destroy(struct mpsc_queue *q)
+{
+    assert(mpsc_queue_acquire(q));
+    free(q);
+}
+
+static void
+test_mpsc_queue_insert_ordered(void)
+{
+    struct mpsc_queue *q = mq_create();
+    struct mpsc_queue_node *node;
+    struct element elements[10];
+    size_t i;
+
+    for (i = 0; i < ARRAY_SIZE(elements); i++) {
+        elements[i].id = i;
+        mpsc_queue_insert(q, &elements[i].node);
+    }
+
+    assert(mpsc_queue_acquire(q));
+
+    i = 0;
+    MPSC_QUEUE_FOR_EACH (node, q) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e == &elements[i]);
+        i++;
+    }
+    assert(i == ARRAY_SIZE(elements));
+
+    MPSC_QUEUE_FOR_EACH_POP (node, q) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e->id == (unsigned int)(e - elements));
+    }
+
+    mpsc_queue_release(q);
+    mq_destroy(q);
+}
+
+static struct mpsc_queue_node *
+mpsc_queue_insert_begin(struct mpsc_queue *queue, struct mpsc_queue_node *node)
+{
+    struct mpsc_queue_node *prev;
+
+    atomic_store_explicit(&node->next, NULL, memory_order_relaxed);
+    prev = atomic_exchange_explicit(&queue->head, node, memory_order_acq_rel);
+    return prev;
+}
+
+static void
+mpsc_queue_insert_end(struct mpsc_queue_node *prev, struct mpsc_queue_node *node)
+{
+    atomic_store_explicit(&prev->next, node, memory_order_release);
+}
+
+static void
+test_mpsc_queue_insert_partial(void)
+{
+    struct mpsc_queue *q = mq_create();
+    struct mpsc_queue_node *node;
+    struct element elements[10];
+    struct mpsc_queue_node *prevs[ARRAY_SIZE(elements)];
+    size_t i;
+
+    for (i = 0; i < ARRAY_SIZE(elements); i++) {
+        elements[i].id = i;
+        if (i > ARRAY_SIZE(elements) / 2) {
+            prevs[i] = mpsc_queue_insert_begin(q, &elements[i].node);
+        } else {
+            prevs[i] = NULL;
+            mpsc_queue_insert(q, &elements[i].node);
+        }
+    }
+
+    assert(mpsc_queue_acquire(q));
+
+    /* Verify that when the chain is broken, iterators will stop. */
+    i = 0;
+    MPSC_QUEUE_FOR_EACH (node, q) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e == &elements[i]);
+        i++;
+    }
+    assert(i < ARRAY_SIZE(elements));
+
+    for (i = 0; i < ARRAY_SIZE(elements); i++) {
+        if (prevs[i] != NULL) {
+            mpsc_queue_insert_end(prevs[i], &elements[i].node);
+        }
+    }
+
+    i = 0;
+    MPSC_QUEUE_FOR_EACH (node, q) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e == &elements[i]);
+        i++;
+    }
+    assert(i == ARRAY_SIZE(elements));
+
+    MPSC_QUEUE_FOR_EACH_POP (node, q) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e->id == (unsigned int)(e - elements));
+    }
+
+    mpsc_queue_release(q);
+    mq_destroy(q);
+}
+
+static void
+test_mpsc_queue_push_back(void)
+{
+    struct mpsc_queue *q = mq_create();
+    struct mpsc_queue_node *node;
+    struct element elements[10];
+    size_t i;
+
+    assert(mpsc_queue_acquire(q));
+
+    assert(mpsc_queue_pop(q) == NULL);
+    mpsc_queue_push_back(q, &elements[0].node);
+    node = mpsc_queue_pop(q);
+    assert(node == &elements[0].node);
+    assert(mpsc_queue_pop(q) == NULL);
+
+    mpsc_queue_push_back(q, &elements[0].node);
+    mpsc_queue_push_back(q, &elements[1].node);
+    assert(mpsc_queue_pop(q) == &elements[1].node);
+    assert(mpsc_queue_pop(q) == &elements[0].node);
+    assert(mpsc_queue_pop(q) == NULL);
+
+    mpsc_queue_push_back(q, &elements[1].node);
+    mpsc_queue_push_back(q, &elements[0].node);
+    mpsc_queue_insert(q, &elements[2].node);
+    assert(mpsc_queue_pop(q) == &elements[0].node);
+    assert(mpsc_queue_pop(q) == &elements[1].node);
+    assert(mpsc_queue_pop(q) == &elements[2].node);
+    assert(mpsc_queue_pop(q) == NULL);
+
+    for (i = 0; i < ARRAY_SIZE(elements); i++) {
+        elements[i].id = i;
+        mpsc_queue_insert(q, &elements[i].node);
+    }
+
+    node = mpsc_queue_pop(q);
+    mpsc_queue_push_back(q, node);
+    assert(mpsc_queue_pop(q) == node);
+    mpsc_queue_push_back(q, node);
+
+    i = 0;
+    MPSC_QUEUE_FOR_EACH (node, q) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e == &elements[i]);
+        i++;
+    }
+    assert(i == ARRAY_SIZE(elements));
+
+    MPSC_QUEUE_FOR_EACH_POP (node, q) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e->id == (unsigned int)(e - elements));
+    }
+
+    mpsc_queue_release(q);
+    mq_destroy(q);
+}
+
+int main(int argc, const char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    test_mpsc_queue_insert_ordered();
+    test_mpsc_queue_insert_partial();
+    test_mpsc_queue_push_back();
+    return 0;
+}
