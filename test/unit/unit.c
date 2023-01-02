grivet/mpsc-queue

commit 72a281bce9b1470f7f27701a7f3888501bdadad3

initial version
Signed-off-by: Gaetan Rivet <grive@u256.net>

src/mpsc-queue.h
@@ -1,0 +1,35 @@
+#ifndef MPSC_QUEUE_H
+#define MPSC_QUEUE_H
+
+#include <stdlib.h>
+#include <stdatomic.h>
+
+struct mpsc_queue_node {
+    _Atomic(struct mpsc_queue_node *) next;
+};
+
+struct mpsc_queue {
+    _Atomic(struct mpsc_queue_node *) head;
+    _Atomic(struct mpsc_queue_node *) tail;
+    struct mpsc_queue_node stub;
+};
+
+/* Producer API. */
+
+void mpsc_queue_insert(struct mpsc_queue *queue, struct mpsc_queue_node *node);
+
+/* Consumer API. */
+
+enum mpsc_queue_poll_result {
+    MPSC_QUEUE_EMPTY,
+    MPSC_QUEUE_ITEM,
+    MPSC_QUEUE_RETRY,
+};
+
+void mpsc_queue_init(struct mpsc_queue *queue);
+void mpsc_queue_destroy(struct mpsc_queue *queue);
+
+enum mpsc_queue_poll_result
+mpsc_queue_poll(struct mpsc_queue *queue, struct mpsc_queue_node **node);
+
+#endif /* MPSC_QUEUE_H */

.editorconfig
@@ -1,0 +1,12 @@
+root = true
+
+[*]
+end_of_line = lf
+insert_final_newline = true
+charset = utf-8
+indent_style = space
+indent_size = 4
+trim_trailing_whitespace = true
+
+[Makefile]
+indent_style = tab

.gitignore
@@ -1,0 +1,6 @@
+config.mk
+obj/*
+lib/*
+*.d
+*.o
+test/test

COPYRIGHT
@@ -1,0 +1,30 @@
+Copyright © 2021 Gaëtan Rivet
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided
+   with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

Makefile
@@ -1,0 +1,57 @@
+# GNU Make
+
+include config.mk
+ifeq ($(CONFIGURED),)
+$(error Please run "./configure" before using make)
+endif
+
+MAKEFLAGS += --jobs=$(shell nproc)
+MAKEFLAGS += --no-print-directory
+
+LIB = libmpsc.so
+
+SRCS = $(sort $(wildcard src/*.c))
+OBJS = $(SRCS:src/%.c=obj/%.o)
+INCLUDES = $(sort $(wildcard src/*.h))
+
+CFLAGS_ALL = -std=c11 -MD $(CFLAGS_AUTO) $(CFLAGS)
+
+all: lib/$(LIB)
+
+lib/$(LIB): $(OBJS) | mkdir-lib
+	$(CC) -shared $(CFLAGS_ALL) $(LDFLAGS_ALL) $^ -o $@
+
+obj/%.o: src/%.c config.mk Makefile | mkdir-obj
+	$(CC) $(CFLAGS_ALL) -c -o $@ $<
+
+INSTALL = $(CURDIR)/tools/install.sh
+
+$(INCLUDEDIR)/%.h: src/%.h
+	$(INSTALL) -D -m 644 $< $@
+
+install-bin: $(LIB:lib/%=$(BINDIR)/%)
+
+install-headers: $(INCLUDES:src/%=$(INCLUDEDIR)/%)
+
+install: install-bin install-headers
+
+TO_BE_CLEANED += lib
+mkdir-lib:
+	@mkdir -p lib
+
+TO_BE_CLEANED += obj
+mkdir-obj:
+	@mkdir -p obj
+
+srcdir := $(CURDIR)
+include test/Makefile
+
+-include obj/*.d
+
+.PHONY: clean
+clean:
+	rm -rf $(TO_BE_CLEANED)
+
+.PHONY: distclean
+distclean: clean
+	rm -f config.mk

configure
@@ -1,0 +1,316 @@
+#!/usr/bin/env sh
+
+# Configuration script ; copied from musl
+
+# Default configuration
+PREFIX=/usr/local
+BINDIR="\$(PREFIX)/bin"
+LIBDIR="\$(PREFIX)/lib"
+INCLUDEDIR="\$(PREFIX)/include"
+
+debug=no
+warnings=no
+shared=yes
+static=yes
+
+usage () {
+cat <<EOF
+Usage: $0 [OPTION]... [VAR=VALUE]
+
+To assign environment variables (e.g., CC, CFLAGS...), specify them as
+VAR=VALUE.  See below for descriptions of some of the useful variables.
+
+Defaults for the options are specified in brackets.
+
+Installation directories:
+  --prefix=PREFIX         main installation prefix [$PREFIX]
+  --bindir=DIR            binary files [$BINDIR]
+  --libdir=DIR            library files [$LIBDIR]
+  --includedir=DIR        include files for the C compiler [$INCLUDEDIR]
+
+Optional features:
+  --enable-debug          build with debugging information [debug=$debug]
+  --enable-warnings       build with recommended warnings flags [warnings=$warnings]
+  --disable-shared        inhibit building shared library [shared=$shared]
+  --disable-static        inhibit building static library [static=$static]
+
+Some influential environment variables:
+  CC                      C compiler command [detected]
+  CFLAGS                  C compiler flags [-Os -pipe ...]
+  CROSS_COMPILE           prefix for cross compiler and tools [none]
+
+Use these variables to override the choices made by configure.
+
+EOF
+exit 0
+}
+
+# Helper functions
+
+quote () {
+tr '\n' ' ' <<EOF | grep '^[-[:alnum:]_=,./:]* $' >/dev/null 2>&1 && { echo "$1" ; return 0 ; }
+$1
+EOF
+printf %s\\n "$1" | sed -e "s/'/'\\\\''/g" -e "1s/^/'/" -e "\$s/\$/'/" -e "s#^'\([-[:alnum:]_,./:]*\)=\(.*\)\$#\1='\2#"
+}
+echo () { printf "%s\n" "$*" ; }
+fail () { echo "$*" ; exit 1 ; }
+fnmatch () { eval "case \"\$2\" in $1) return 0 ;; *) return 1 ;; esac" ; }
+cmdexists () { command -v "$1" >/dev/null 2>&1 ; }
+trycc () { test -z "$CC" && cmdexists "$1" && CC=$1 ; }
+
+stripdir () {
+while eval "fnmatch '*/' \"\${$1}\"" ; do eval "$1=\${$1%/}" ; done
+}
+
+trycppif () {
+printf "checking preprocessor condition %s... " "$1"
+{
+echo "typedef int x;"
+echo "#if $1"
+echo "#error yes"
+echo "#endif"
+} > "$tmpc"
+if $CC $2 -c -o /dev/null "$tmpc" >/dev/null 2>&1 ; then
+printf "false\n"
+return 1
+else
+printf "true\n"
+return 0
+fi
+}
+
+tryflag () {
+printf "checking whether compiler accepts %s... " "$2"
+echo "typedef int x;" > "$tmpc"
+if $CC $CFLAGS_TRY $2 -c -o /dev/null "$tmpc" >/dev/null 2>&1 ; then
+printf "yes\n"
+eval "$1=\"\${$1} \$2\""
+eval "$1=\${$1# }"
+return 0
+else
+printf "no\n"
+return 1
+fi
+}
+
+tryldflag () {
+printf "checking whether linker accepts %s... " "$2"
+echo "typedef int x;" > "$tmpc"
+if $CC $LDFLAGS_TRY -nostdlib -shared $2 -o /dev/null "$tmpc" >/dev/null 2>&1 ; then
+printf "yes\n"
+eval "$1=\"\${$1} \$2\""
+eval "$1=\${$1# }"
+return 0
+else
+printf "no\n"
+return 1
+fi
+}
+
+# Beginning of actual script
+
+for arg ; do
+case "$arg" in
+--help|-h) usage ;;
+--prefix=*) PREFIX=${arg#*=} ;;
+--bindir=*) BINDIR=${arg#*=} ;;
+--libdir=*) LIBDIR=${arg#*=} ;;
+--includedir=*) INCLUDEDIR=${arg#*=} ;;
+--enable-shared|--enable-shared=yes) shared=yes ;;
+--disable-shared|--enable-shared=no) shared=no ;;
+--enable-static|--enable-static=yes) static=yes ;;
+--disable-static|--enable-static=no) static=no ;;
+--enable-debug|--enable-debug=yes) debug=yes ;;
+--disable-debug|--enable-debug=no) debug=no ;;
+--enable-warnings|--enable-warnings=yes) warnings=yes ;;
+--disable-warnings|--enable-warnings=no) warnings=no ;;
+-* ) echo "$0: unknown option $arg" ;;
+AR=*) AR=${arg#*=} ;;
+CC=*) CC=${arg#*=} ;;
+CFLAGS=*) CFLAGS=${arg#*=} ;;
+CPPFLAGS=*) CPPFLAGS=${arg#*=} ;;
+LDFLAGS=*) LDFLAGS=${arg#*=} ;;
+CROSS_COMPILE=*) CROSS_COMPILE=${arg#*=} ;;
+esac
+done
+
+for i in PREFIX BINDIR LIBDIR INCLUDEDIR ; do
+stripdir $i
+done
+
+#
+# Get a temp filename we can use
+#
+i=0
+set -C
+while : ; do i=$((i+1))
+tmpc="./conf$$-$PPID-$i.c"
+: 2>/dev/null > "$tmpc" && break
+test "$i" -gt 50 && fail "$0: cannot create temporary file $tmpc"
+done
+set +C
+trap 'rm "$tmpc"' EXIT INT QUIT TERM HUP
+
+#
+# Find a C compiler to use
+#
+printf "checking for C compiler... "
+trycc "${CROSS_COMPILE}clang"
+trycc "${CROSS_COMPILE}gcc"
+trycc "${CROSS_COMPILE}cc"
+printf "%s\n" "$CC"
+test -n "$CC" || { echo "$0: cannot find a C compiler" ; exit 1 ; }
+
+printf "checking whether C compiler works... "
+echo "typedef int x;" > "$tmpc"
+if output="$($CC -c -o /dev/null "$tmpc" 2>&1)" ; then
+printf "yes\n"
+else
+printf "no; compiler output follows:\n%s\n" "$output"
+exit 1
+fi
+
+
+CFLAGS_AUTO=""
+LDFLAGS_AUTO=""
+
+#
+# Figure out options to force errors on unknown flags.
+#
+tryflag   CFLAGS_TRY  -Werror=unknown-warning-option
+tryflag   CFLAGS_TRY  -Werror=unused-command-line-argument
+tryflag   CFLAGS_TRY  -Werror=ignored-optimization-argument
+tryldflag LDFLAGS_TRY -Werror=unknown-warning-option
+tryldflag LDFLAGS_TRY -Werror=unused-command-line-argument
+
+#
+# Enable debugging if requested.
+#
+test "x$debug" = xyes && CFLAGS_AUTO=-g3
+
+#
+# Possibly add a -O option to CFLAGS
+#
+printf "checking for optimization settings... "
+if fnmatch '-O*|*\ -O*' "$CFLAGS_AUTO $CFLAGS" ; then
+printf "using provided CFLAGS\n"
+else
+printf "using defaults\n" ; tryflag CFLAGS_AUTO -O2
+fi
+
+# Always try -pipe
+tryflag CFLAGS_AUTO -pipe
+
+#
+# If debugging is disabled, omit frame pointer. Modern GCC does this
+# anyway on most archs even when debugging is enabled since the frame
+# pointer is no longer needed for debugging.
+#
+if fnmatch '-g*|*\ -g*' "$CFLAGS_AUTO $CFLAGS" ; then :
+else 
+tryflag CFLAGS_AUTO -fomit-frame-pointer
+fi
+
+#
+# Modern GCC wants to put DWARF tables (used for debugging and
+# unwinding) in the loaded part of the program where they are
+# unstrippable. These options force them back to debug sections (and
+# cause them not to get generated at all if debugging is off).
+#
+tryflag CFLAGS_AUTO -fno-unwind-tables
+tryflag CFLAGS_AUTO -fno-asynchronous-unwind-tables
+
+#
+# Attempt to put each function and each data object in its own
+# section. This both allows additional size optimizations at link
+# time and works around a dangerous class of compiler/assembler bugs
+# whereby relative address expressions are constant-folded by the
+# assembler even when one or more of the symbols involved is
+# replaceable. See gas pr 18561 and gcc pr 66609, 68178, etc.
+#
+tryflag CFLAGS_AUTO -ffunction-sections
+tryflag CFLAGS_AUTO -fdata-sections
+
+#
+# Even with -std=c99, gcc accepts some constructs which are constraint
+# violations. We want to treat these as errors regardless of whether
+# other purely stylistic warnings are enabled -- especially implicit
+# function declarations, which are a dangerous programming error.
+#
+tryflag CFLAGS_AUTO -Werror=implicit-function-declaration
+tryflag CFLAGS_AUTO -Werror=implicit-int
+tryflag CFLAGS_AUTO -Werror=pointer-sign
+tryflag CFLAGS_AUTO -Werror=pointer-arith
+
+#
+# GCC ignores unused arguments by default, but Clang needs this extra
+# parameter to stop printing warnings about LDFLAGS passed during
+# compiling stage and CFLAGS passed during linking stage.
+#
+tryflag CFLAGS_AUTO -Qunused-arguments
+
+if test "x$warnings" = xyes ; then
+tryflag CFLAGS_AUTO -Wall
+tryflag CFLAGS_AUTO -Wextra
+tryflag CFLAGS_AUTO -Wno-parentheses
+tryflag CFLAGS_AUTO -Wno-uninitialized
+tryflag CFLAGS_AUTO -Wno-missing-braces
+tryflag CFLAGS_AUTO -Wno-unused-value
+tryflag CFLAGS_AUTO -Wno-unused-but-set-variable
+tryflag CFLAGS_AUTO -Wno-unknown-pragmas
+tryflag CFLAGS_AUTO -Wno-pointer-to-int-cast
+fi
+
+# Reduce space lost to padding for alignment purposes by sorting data
+# objects according to their alignment requirements. This approximates
+# optimal packing.
+tryldflag LDFLAGS_AUTO -Wl,--sort-section,alignment
+tryldflag LDFLAGS_AUTO -Wl,--sort-common
+
+# When linking shared library, drop dummy weak definitions that were
+# replaced by strong definitions from other translation units.
+tryldflag LDFLAGS_AUTO -Wl,--gc-sections
+
+# Some patched GCC builds have these defaults messed up...
+tryldflag LDFLAGS_AUTO -Wl,--hash-style=both
+
+# Prevent linking if there are undefined symbols
+tryldflag LDFLAGS_AUTO -Wl,--no-undefined
+
+# Avoid exporting symbols from compiler runtime libraries. They
+# should be hidden anyway, but some toolchains including old gcc
+# versions built without shared library support and pcc are broken.
+tryldflag LDFLAGS_AUTO -Wl,--exclude-libs=ALL
+
+printf "creating config.mk... "
+
+cmdline=$(quote "$0")
+for i ; do cmdline="$cmdline $(quote "$i")" ; done
+
+exec 3>&1 1>config.mk
+
+cat << EOF
+# This version of config.mk was generated by:
+# $cmdline
+# Any changes made here will be lost if configure is re-used
+AR = ${AR:-\$(CROSS_COMPILE)ar}
+RANLIB = ${RANLIB:-\$(CROSS_COMPILE)ranlib}
+PREFIX = $PREFIX
+BINDIR = $BINDIR
+LIBDIR = $LIBDIR
+INCLUDEDIR = $INCLUDEDIR
+CC = $CC
+CFLAGS = $CFLAGS
+CFLAGS_AUTO = $CFLAGS_AUTO
+LDFLAGS = $LDFLAGS
+LDFLAGS_AUTO = $LDFLAGS_AUTO
+CROSS_COMPILE = $CROSS_COMPILE
+
+CONFIGURED = yes
+EOF
+test "x$static" = xno && echo "STATIC_LIBS ="
+test "x$shared" = xno && echo "SHARED_LIBS ="
+exec 1>&3 3>&-
+
+printf "done\n"

src/mpsc-queue.c
@@ -1,0 +1,74 @@
+#include <stdlib.h>
+#include <stdatomic.h>
+
+#include "mpsc-queue.h"
+
+/* Consumer API. */
+
+void
+mpsc_queue_init(struct mpsc_queue *queue)
+{
+    atomic_store_explicit(&queue->head, &queue->stub, memory_order_relaxed);
+    atomic_store_explicit(&queue->tail, &queue->stub, memory_order_relaxed);
+    atomic_store_explicit(&queue->stub.next, NULL, memory_order_relaxed);
+}
+
+void
+mpsc_queue_destroy(struct mpsc_queue *queue)
+{
+}
+
+enum mpsc_queue_poll_result
+mpsc_queue_poll(struct mpsc_queue *queue, struct mpsc_queue_node **node)
+{
+    struct mpsc_queue_node *tail;
+    struct mpsc_queue_node *next;
+    struct mpsc_queue_node *head;
+
+    tail = atomic_load_explicit(&queue->tail, memory_order_relaxed);
+    next = atomic_load_explicit(&tail->next, memory_order_acquire);
+
+    if (tail == &queue->stub) {
+        if (next == NULL) {
+            return MPSC_QUEUE_EMPTY;
+        }
+
+        atomic_store_explicit(&queue->tail, next, memory_order_relaxed);
+        tail = next;
+        next = atomic_load_explicit(&tail->next, memory_order_acquire);
+    }
+
+    if (next != NULL) {
+        atomic_store_explicit(&queue->tail, next, memory_order_relaxed);
+        *node = tail;
+        return MPSC_QUEUE_ITEM;
+    }
+
+    head = atomic_load_explicit(&queue->head, memory_order_acquire);
+    if (tail != head) {
+        return MPSC_QUEUE_RETRY;
+    }
+
+    mpsc_queue_insert(queue, &queue->stub);
+
+    next = atomic_load_explicit(&tail->next, memory_order_acquire);
+    if (next != NULL) {
+        atomic_store_explicit(&queue->tail, next, memory_order_relaxed);
+        *node = tail;
+        return MPSC_QUEUE_ITEM;
+    }
+
+    return MPSC_QUEUE_EMPTY;
+}
+
+/* Producer API. */
+
+void
+mpsc_queue_insert(struct mpsc_queue *queue, struct mpsc_queue_node *node)
+{
+    struct mpsc_queue_node *prev;
+
+    atomic_store_explicit(&node->next, NULL, memory_order_relaxed);
+    prev = atomic_exchange_explicit(&queue->head, node, memory_order_acq_rel);
+    atomic_store_explicit(&prev->next, node, memory_order_release);
+}

test/Makefile
@@ -1,0 +1,31 @@
+# GNU Make
+
+srcdir ?= ..
+
+SRCS = $(sort $(wildcard test/*.c))
+OBJS = $(SRCS:%.c=%.o)
+
+TO_BE_CLEANED += $(OBJS)
+
+include $(srcdir)/config.mk
+
+TEST = test/test
+
+test/%.o: test/%.c | lib/$(LIB) Makefile
+	$(CC) -pthread -std=c11 -MD -Wall -Wextra $(CFLAGS) -c -I$(srcdir)/src -o $@ $<
+
+$(TEST): $(OBJS)
+	$(CC) -pthread -L$(srcdir)/lib -o $@ $^ -lmpsc
+
+test: $(TEST)
+
+run: test
+	LD_LIBRARY_PATH=$(srcdir)/lib $(WRAPPER) $(TEST)
+
+-include obj/*.d
+-include test/*.d
+
+TO_BE_CLEANED += test/*.d
+TO_BE_CLEANED += test/test
+
+.PHONY: all

commit 860546d76cceb0439f46d03f67b0a496b23b055e

mpsc-queue: Fix poll return value on inconsistent reads
If a node has been partially inserted, the value returned by the poll
function should be 'MPSC_QUEUE_RETRY'.

This property is currently not tested, and it was found false.

Add the related unit test and fix the implementation.

Fixes: 72a281bce9b1 ("initial version")
Signed-off-by: Gaetan Rivet <grive@u256.net>

mpsc-queue.h
@@ -94,7 +94,12 @@ mpsc_queue_poll(struct mpsc_queue *queue, struct mpsc_queue_node **node)
 
     if (tail == &queue->stub) {
         if (next == NULL) {
-            return MPSC_QUEUE_EMPTY;
+            head = atomic_load_explicit(&queue->head, memory_order_acquire);
+            if (tail != head) {
+                return MPSC_QUEUE_RETRY;
+            } else {
+                return MPSC_QUEUE_EMPTY;
+            }
         }
 
         atomic_store_explicit(&queue->tail, next, memory_order_relaxed);

test/unit/unit.c
@@ -122,6 +122,30 @@ test_mpsc_queue_insert_partial(void)
 }
 
 static void
+test_mpsc_queue_poll(void)
+{
+    struct mpsc_queue *q = mq_create();
+    struct mpsc_queue_node *node;
+    struct element elements[1];
+    struct mpsc_queue_node *prevs[ARRAY_SIZE(elements)];
+    size_t i;
+
+    for (i = 0; i < ARRAY_SIZE(elements); i++) {
+        elements[i].id = i;
+    }
+
+    prevs[0] = mpsc_queue_insert_begin(q, &elements[0].node);
+    assert(mpsc_queue_poll(q, &node) == MPSC_QUEUE_RETRY);
+    assert(mpsc_queue_poll(q, &node) == MPSC_QUEUE_RETRY);
+
+    mpsc_queue_insert_end(prevs[0], &elements[0].node);
+    assert(mpsc_queue_poll(q, &node) == MPSC_QUEUE_ITEM);
+    assert(mpsc_queue_poll(q, &node) == MPSC_QUEUE_EMPTY);
+
+    mq_destroy(q);
+}
+
+static void
 test_mpsc_queue_push_front(void)
 {
     struct mpsc_queue *q = mq_create();
@@ -179,6 +203,7 @@ int main(void)
 {
     test_mpsc_queue_insert_ordered();
     test_mpsc_queue_insert_partial();
+    test_mpsc_queue_poll();
     test_mpsc_queue_push_front();
     return 0;
 }