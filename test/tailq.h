6cd17a964c95d3397e43cde728d0ea8e32bedef2
Gaetan Rivet <grive@u256.net>
2023-02-02 16:33:24 +0100

    tailq: Always use mutex
    Signed-off-by: Gaetan Rivet <grive@u256.net>

--- test/tailq.h
+++ test/tailq.h
@@ -5,21 +5,23 @@
 #include <sys/queue.h>
 
 #if __APPLE__
+/* No Spinlock on macos. */
 #define USE_FAIR_LOCK
 #endif
+#define USE_FAIR_LOCK
 
-#ifndef USE_FAIR_LOCK
-#define tailq_lock_type pthread_spinlock_t
-#define tailq_lock_init(l) do { pthread_spin_init(l, PTHREAD_PROCESS_PRIVATE); } while (0)
-#define tailq_lock_destroy(l) do { pthread_spin_destroy(l); } while (0)
-#define tailq_lock(l) do { pthread_spin_lock(l); } while (0)
-#define tailq_unlock(l) do { pthread_spin_unlock(l); } while (0)
-#else
+#ifdef USE_FAIR_LOCK
 #define tailq_lock_type pthread_mutex_t
 #define tailq_lock_init(l) do { pthread_mutex_init(l, NULL); } while (0)
 #define tailq_lock_destroy(l) do { pthread_mutex_destroy(l); } while (0)
 #define tailq_lock(l) do { pthread_mutex_lock(l); } while (0)
 #define tailq_unlock(l) do { pthread_mutex_unlock(l); } while (0)
+#else
+#define tailq_lock_type pthread_spinlock_t
+#define tailq_lock_init(l) do { pthread_spin_init(l, PTHREAD_PROCESS_PRIVATE); } while (0)
+#define tailq_lock_destroy(l) do { pthread_spin_destroy(l); } while (0)
+#define tailq_lock(l) do { pthread_spin_lock(l); } while (0)
+#define tailq_unlock(l) do { pthread_spin_unlock(l); } while (0)
 #endif
 
 struct tailq_node {
