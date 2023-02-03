b7c1aff165b433a7734b22705a99e867bdbfc5a2
Gaetan Rivet <grive@u256.net>
2023-02-03 17:33:39 +0100

    unit: Add mpscq unit tests
    Signed-off-by: Gaetan Rivet <grive@u256.net>

--- Makefile
+++ Makefile
@@ -14,18 +14,20 @@ CFLAGS_ALL += -I$(CURDIR) -I$(CURDIR)/test
 test/%.o: test/%.c Makefile
 	$(CC) $(CFLAGS_ALL) -c -o $@ $<
 
+test_OBJS := test/util.o
+test_OBJS += test/tailq.o
+test_OBJS += test/mpsc-queue.o
+test_OBJS += test/ts-mpsc-queue.o
+
 unit_OBJS := test/unit/main.o
 unit_OBJS += test/unit/mpsc-queue.o
-unit_OBJS += test/util.o
+unit_OBJS += $(test_OBJS)
 
 unit: $(unit_OBJS)
 	$(CC) $(CFLAGS_ALL) -o $@ $^
 
 perf_OBJS := test/perf/main.o
-perf_OBJS += test/util.o
-perf_OBJS += test/mpsc-queue.o
-perf_OBJS += test/tailq.o
-perf_OBJS += test/ts-mpsc-queue.o
+perf_OBJS += $(test_OBJS)
 ifeq ($(UNAME_S),Darwin)
 perf_OBJS += test/perf/pthread-barrier.o
 endif
--- test/unit/main.c
+++ test/unit/main.c
@@ -1,7 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#include "mpscq.h"
 #include "unit.h"
+#include "util.h"
+
+struct element {
+    unsigned int id;
+    union mpscq_node node;
+};
+
+static void
+test_mpscq_insert(struct mpscq *q)
+{
+    union mpscq_node *node;
+    struct element elements[10];
+    size_t i;
+
+    mpscq_init(q);
+
+    for (i = 0; i < ARRAY_SIZE(elements); i++) {
+        elements[i].id = i;
+        mpscq_insert(q, &elements[i].node);
+    }
+
+    while ((node = mpscq_pop(q))) {
+        struct element *e = container_of(node, struct element, node);
+        assert(e->id == (unsigned int)(e - elements));
+    }
+}
 
 int main(void)
 {
+    test_mpscq_insert(&ts_mpsc_queue);
+    test_mpscq_insert(&tailq);
+    test_mpscq_insert(&mpsc_queue);
     test_mpsc_queue();
     return 0;
 }
